752f440906d6a90ac37964611c845dd79305d399
Gilbert Gilb's <gilbsgilbert@gmail.com>
2019-06-24 00:54:05 +0200

    Refactor and simplify test runner.

--- tests/runner.ts
+++ tests/runner.ts
@@ -22,8 +22,8 @@ interface TestData {
   // All JS files are applied one after another.
   inputFiles: string[];
 
-  // list of [["key", "path"], "value", {ns: 'namespace', 'locale': 'fr'}]
-  expectValues: [string[], string, ExpectKeysOpts?][];
+  // list of [expected, {ns: 'namespace', 'locale': 'fr'}]
+  expectValues: [string, ExpectKeysOpts?][];
 
   pluginOptions: Partial<Config>;
 }
@@ -55,9 +55,13 @@ function* genTestData(): IterableIterator<TestData> {
     for (const testFileEnt of testFilesEnt) {
       // testFile is a JSON file
       const testFile = path.join(testDir, testFileEnt.name);
-      const testData = fs.readJSONSync(testFile, {
+      const rawTestData = fs.readJSONSync(testFile, {
         encoding: 'utf8',
-      }) as Partial<TestData>;
+      });
+      if (!Array.isArray(rawTestData.expectValues)) {
+        rawTestData.expectValues = [[rawTestData.expectValues]];
+      }
+      const testData: Partial<TestData> = rawTestData;
 
       const inputFiles =
         testData.inputFiles === undefined
@@ -93,114 +97,30 @@ function* genTestData(): IterableIterator<TestData> {
   }
 }
 
-type ReadKeyPathType = (
-  keyPath: string[],
-  opts?: ExpectKeysOpts,
-) => string | number | null | undefined;
-type CountKeysType = (opts?: ExpectKeysOpts) => number;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function readExtractedFile(outputPath: string, opts?: ExpectKeysOpts): any {
+  const ns = (opts && opts.ns) || 'translation';
+  const locale = (opts && opts.locale) || 'en';
 
-/**
- * Cached reading of extracted files.
- * e.g.
- *
- * cachedReader('translations/{{ns}}/{{locale}}.json', (readKeyPath, countKeys) => {
- *   const keyVal = readKeyPath(['some', 'key', 'path'], {ns: 'namespace', locale: 'fr'});
- *   const countKeys = countKeys({ns: 'namespace', locale: 'fr'});
- * })
- */
-function cachedReader<T>(
-  outputPath: string,
-  cb: (readKeyPath: ReadKeyPathType, countKeys: CountKeysType) => T,
-): T {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const cache: { [key: string]: any } = {};
-  const cacheKeySep = '\0';
+  const realOutputPath = outputPath
+    .replace('{{ns}}', ns)
+    .replace('{{locale}}', locale);
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const readOutput = (opts?: ExpectKeysOpts): any => {
-    const ns = (opts && opts.ns) || 'translation';
-    const locale = (opts && opts.locale) || 'en';
-
-    const cacheKey = [ns, locale].join(cacheKeySep);
-    if (cacheKey in cache) return cache[cacheKey];
-
-    const realOutputPath = outputPath
-      .replace('{{ns}}', ns)
-      .replace('{{locale}}', locale);
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    let extracted: any;
-    try {
-      extracted = fs.readJSONSync(realOutputPath, { encoding: 'utf8' });
-    } catch (err) {
-      if (err.code !== 'ENOENT') throw err;
-      cache[cacheKey] = undefined;
-      return undefined;
-    }
-    cache[cacheKey] = extracted;
-    return extracted;
-  };
-
-  const findKeyPath = (
-    keyPath: string[],
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    data: any,
-  ): number | string | null | undefined => {
-    let obj = data;
-    for (const node of keyPath) {
-      if (obj !== Object(obj)) return undefined;
-      obj = obj[node];
-      if (obj === undefined) return obj;
-    }
-    return obj;
-  };
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const countKeysOnData = (data: any): number => {
-    if (Array.isArray(data) || Object(data) !== data) return 1;
-    return Object.values(data).reduce(
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      (prev: number, curr: any) => prev + countKeysOnData(curr),
-      0,
-    );
-  };
-
-  const countKeys: CountKeysType = opts => {
-    return countKeysOnData(readOutput(opts));
-  };
-
-  const readKeyPath: ReadKeyPathType = (keyPath, opts) => {
-    return findKeyPath(keyPath, readOutput(opts));
-  };
-
-  return cb(readKeyPath, countKeys);
+  let extracted: any = fs.readJSONSync(realOutputPath, { encoding: 'utf8' });
+  return extracted;
 }
 
-function testHasExpectedValues(
-  testData: TestData,
-  readKeyPath: ReadKeyPathType,
-  countKeys: CountKeysType,
-): void {
+function assertHasExpectedValues(testData: TestData): void {
   it(`should have all expected values`, () => {
-    for (const [keyPath, value, opts] of testData.expectValues) {
-      const v = readKeyPath(keyPath, opts);
-      expect(
-        v,
-        `Couldn't find keyPath ${keyPath}. opts=${JSON.stringify(opts)}`,
-      ).not.toBeUndefined();
-      expect(
-        v,
-        `Unexpected value for keyPath ${keyPath}. opts=${JSON.stringify(
-          opts,
-        )}`,
-      ).toEqual(value);
+    for (const [expected, opts] of testData.expectValues) {
+      const extracted = readExtractedFile(
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        testData.pluginOptions.outputPath!,
+        opts,
+      );
+      expect(extracted, `opts=${JSON.stringify(opts)}`).toEqual(expected);
     }
-    const numberOfExtractedKeys = countKeys();
-    expect(
-      numberOfExtractedKeys,
-      `Too many keys were extracted. Please ensure that you put all ` +
-        `extracted keys in "expectValues".`,
-    ).not.toBeGreaterThan(testData.expectValues.length);
   });
 }
 
@@ -223,13 +143,7 @@ export function runChecks(): void {
         ).toBeTruthy();
       }
 
-      cachedReader(
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        testData.pluginOptions!.outputPath!,
-        (readKeyPath, countKeys) => {
-          testHasExpectedValues(testData, readKeyPath, countKeys);
-        },
-      );
+      assertHasExpectedValues(testData);
     });
   }
 }
