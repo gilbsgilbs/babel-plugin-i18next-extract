101be45c47b2292c6e4a74d179a0808024f2f41d
Peter 'Pita' Martischka <peter.martischka@produktmacher.com>
2019-09-23 16:02:20 +0200

    Correctly remove all empty jsx text nodes

---
+++ tests/__fixtures__/testTransComponent/singleSpaces.js
@@ -0,0 +1,21 @@
+import { Trans } from "react-i18next";
+
+<Trans i18nKey="key1">
+  I read the{" "}
+  <a
+    href="https://www.polymore.com/tos"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    TOS
+  </a>{" "}
+  and the{" "}
+  <a
+    href="https://www.polymore.com/privacy"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Privacy Policy
+  </a>{" "}
+  and accept them.
+</Trans>;
