13e808c565871ff6740fbb7b759bcdf415a91aa6
Gilbert Gilb's <gilbsgilbert@gmail.com>
2023-01-22 18:38:09 +0100

    fix rollup build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,7 @@
 import { babel } from '@rollup/plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
-import pkg from './package.json';
+import pkg from './package.json' assert { type: 'json' };
 
 const extensions = ['.js', '.ts'];
 
