324aa2e8072b344e7a30853915a99d6b146668cd
Gilbert Gilb's <gilbsgilbert@gmail.com>
2024-12-08 00:24:01 +0100

    chore: update deps, migrate ESLint to this awful "flat" wtf 🤢 format

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,38 +1,38 @@
-import { babel } from '@rollup/plugin-babel';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+import pkg from "./package.json" assert { type: "json" };
 
-import pkg from './package.json' assert { type: 'json' };
+import { babel } from "@rollup/plugin-babel";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 
-const extensions = ['.js', '.ts'];
+const extensions = [".js", ".ts"];
 
 export default {
   input: {
-    index: 'src/index.ts',
+    index: "src/index.ts",
   },
   output: [
     {
-      dir: 'lib/',
-      format: 'cjs',
-      exports: 'named',
+      dir: "lib/",
+      format: "cjs",
+      exports: "named",
     },
     {
-      dir: 'lib/es/',
-      format: 'es',
-      exports: 'named',
+      dir: "lib/es/",
+      format: "es",
+      exports: "named",
     },
   ],
   external: [
-    'fs',
-    'path',
+    "fs",
+    "path",
     ...Object.keys(pkg.dependencies || {}),
     ...Object.keys(pkg.peerDependencies || {}),
   ],
   plugins: [
     nodeResolve({ extensions }),
     babel({
-      include: 'src/**/*',
-      exclude: 'node_modules/**',
-      babelHelpers: 'bundled',
+      include: "src/**/*",
+      exclude: "node_modules/**",
+      babelHelpers: "bundled",
       extensions,
     }),
   ],
